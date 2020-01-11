561ab761cb36d34a99c8a1f2c31ed4c9903405bf
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2020-01-11 13:47:10 +0100

    Test

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
+
+
 let file = document.getElementById("theFile");
 let audio = document.getElementById("audio");
 
+
 file.onchange = function(){
- 
-  let files = this.files;
-  audio.src = URL.createObjectURL(files[0]);
+  let files = this.files; 
+  audio.src = URL.createObjectURL(files[0]); 
   console.log(files);
+
   audio.load();
   audio.play();
 
-  let audioCtx = new AudioContext();
-  let src = audioCtx.createMediaElementSource(audio)
-  let analyser = audioCtx.createAnalyser();
+  let audioCtx = new AudioContext(); // Vytvoření objektu 
+  let src = audioCtx.createMediaElementSource(audio); // Vytovoření 
+  let analyser = audioCtx.createAnalyser(); // Vytvoříme tzv. AnalyserNode, který nám převádí data z audia 
 
   let canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  let ctx = canvas.getContext("2d");
-  
-  src.connect(analyser);
+  canvas.width = window.innerWidth; // Získáme aktualní šířku obrazovky uživatele
+  canvas.height = window.innerHeight; // Získéme aktualní výšku obrazovky uživatele
+  let ctx = canvas.getContext("2d"); 
+
+  src.connect(analyser); // 
   analyser.connect(audioCtx.destination);
 
-  analyser.fftSize = 256;
+  analyser.fftSize = 1024;
   let bufferLength = analyser.frequencyBinCount;
   console.log(bufferLength);
 
@@ -30,10 +33,9 @@ file.onchange = function(){
   let width = canvas.width;
   let height = canvas.height;
 
-  let barWidth = (width / bufferLength) * 2.8;
+  let barWidth = (width / bufferLength) *2.5 ;
   let barHeight;
-  let x;
-
+  let x = 0;
   function draw(){
     requestAnimationFrame(draw);
 
@@ -45,7 +47,7 @@ file.onchange = function(){
     ctx.fillRect(0,0,width,height);
 
     for(let i = 0; i < bufferLength; i++ ){
-      barHeight = dataArray[i] *2;
+      barHeight = dataArray[i] * 1.5;
       let r = barHeight + (25 * (i / bufferLength));
       let g = 250 * (i/bufferLength);
       let b = 50;
@@ -62,5 +64,4 @@ file.onchange = function(){
   draw();
 
 }
-
 
