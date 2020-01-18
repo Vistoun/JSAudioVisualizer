633b84a2f6a342ff8857649042d2cf03efa514a2
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2020-01-18 22:47:35 +0100

    MediaTags implemenation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ let audio = document.getElementById("audio");
 let button = document.getElementById("button");
 let sliderA = document.getElementById("sliderA");
 let sliderW = document.getElementById("sliderW");
+let canvas = document.getElementById("canvas");
+let title = document.getElementById("title");
+canvas.width = window.innerWidth; // Získáme aktualní šířku obrazovky uživatele
+canvas.height = window.innerHeight; // Získéme aktualní výšku obrazovky uživatele
+let ctx = canvas.getContext("2d");
 
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 file.onchange = function () {
 
   let files = this.files;
@@ -17,34 +24,27 @@ file.onchange = function () {
   let src = audioCtx.createMediaElementSource(audio);
   let analyser = audioCtx.createAnalyser(); // Vytvoříme tzv. AnalyserNode, který nám převádí data z audia 
 
-  let canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth; // Získáme aktualní šířku obrazovky uživatele
-  canvas.height = window.innerHeight; // Získéme aktualní výšku obrazovky uživatele
-  let ctx = canvas.getContext("2d");
-
   src.connect(analyser);
   analyser.connect(audioCtx.destination);
   analyser.minDecibels = -90;
   analyser.maxDecibels = -10;
   analyser.smoothingTimeConstant = 0.85;
 
+
   function draw() {
     analyser.fftSize = 1024;
     let bufferLength = analyser.frequencyBinCount;
     console.log(bufferLength);
-
     let dataArray = new Uint8Array(bufferLength);
 
     let width = canvas.width;
     let height = canvas.height;
-
     let barWidth = (width / bufferLength) * updateSliderW();
     let barHeight;
     let x;
 
     requestAnimationFrame(draw);
     x = 0;
-
     analyser.getByteFrequencyData(dataArray);
 
     ctx.fillStyle = "#000";
@@ -55,7 +55,7 @@ file.onchange = function () {
       let r = barHeight + (25 * (i / bufferLength));
       let g = 250 * (i / bufferLength);
       let b = 50;
-     
+
 
       ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
       ctx.fillRect(x, height - barHeight, barWidth, barHeight);
@@ -64,24 +64,18 @@ file.onchange = function () {
 
     }
   }
-  function updateSliderA(){
-    return(sliderA.value);
-  }
-  function updateSliderW(){
-    return(sliderW.value);
-  }
   function toggle() {
     if (button.value == "OFF") {
       button.value = "ON";
       navigator.getUserMedia({ audio: true }, function (stream) {
-        window.localStream = stream;
-        src = audioCtx.createMediaStreamSource(stream); // Vytovoření 
+        localStream = stream;
+        src = audioCtx.createMediaStreamSource(stream);
         audio.pause();
-        src.connect(analyser); //
+        src.connect(analyser);
         analyser.connect(audioCtx.destination);
         analyser.minDecibels = -90;
         analyser.maxDecibels = -10;
-        analyser.smoothingTimeConstant = 0.85;
+        //analyser.smoothingTimeConstant = 0.85;
         draw();
       }, function (err) {
         console.log('The following gUM error occured: ' + err);
@@ -92,14 +86,27 @@ file.onchange = function () {
       localStream.getAudioTracks()[0].stop();
     }
   }
-  button.addEventListener("click", function () {
-    toggle();
-  });
-  sliderA.addEventListener("onchange", function(){
-    updateSliderA();
-  })
-  sliderW.addEventListener("onchange", function(){
-    updateSliderW();
-  })
+
+  function updateSliderA() { return (sliderA.value); }
+  function updateSliderW() { return (sliderW.value); }
+  button.addEventListener("click", function () { toggle(); });
+  sliderA.addEventListener("onchange", function () { updateSliderA(); })
+  sliderW.addEventListener("onchange", function () { updateSliderW(); })
   draw();
 }
+
+file.addEventListener("change", function (event) {
+  let file = event.target.files[0];
+  jsmediatags.read(file, {
+    onSuccess: function (tag) {
+      console.log(tag);
+      let tags = tag.tags;
+      console.log(tags);
+      title.innerHTML = tags.title
+    },
+    onError: function (error) {
+      console.log(':(', error.type, error.info);
+    }
+  });
+}, false);
+
